a116424d1ac02c594f7dca97bb4935f19b8ce990
Rafael <rafael.martinezcalvo@orange.fr>
2021-07-21 14:18:55 +0200

    In rollup mode don't reload on change but on save

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,20 @@ import { loadSvelteCode } from "./svelte-tools";
 export function activate(context: vscode.ExtensionContext) {
   loadSvelteCode(context);
 
+  vscode.workspace.onDidSaveTextDocument((document) => {
+    if (document.fileName.endsWith(".svelte")) {
+      PreviewPanel.panels.get(document.fileName)?.update();
+    }
+  });
   vscode.workspace.onDidChangeTextDocument(({ contentChanges, document }) => {
     if (document.fileName.endsWith(".svelte")) {
+      const { rollup: useRollup } = context.workspaceState.get(
+        "svelte-preview-config",
+        {
+          rollup: false,
+        }
+      );
+      if (useRollup) return;
       PreviewPanel.panels.get(document.fileName)?.update();
     }
   });
