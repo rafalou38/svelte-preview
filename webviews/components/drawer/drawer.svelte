0718c171f6fb77db01cf05b601fef6753a105953
Rafael <rafael.martinezcalvo@orange.fr>
2021-07-26 14:47:57 +0200

    add button to clear console

--- webviews/components/drawer/drawer.svelte
+++ webviews/components/drawer/drawer.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { code, log } from '../stores';
+	import { code, log } from '../stores';
 
 	import Console from './console.svelte'
 	import Errors from './errors.svelte'
@@ -41,6 +41,19 @@ import { code, log } from '../stores';
 			<div class="count">{$code?.err.length}</div>
 		{/if}
 		<li class="tab" class:active={current == "infos"} on:click={()=>current = "infos"}>infos</li>
+		<div class="right">
+			{#if current === "console"}
+				 <button class="action-btn" title="clear console" on:click={()=>$log = []}>
+					 <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M10.0001 12.6L10.7001 13.3L12.3001 11.7L13.9001 13.3L14.7001 12.6L13.0001 11L14.7001 9.40005L13.9001 8.60005L12.3001 10.3L10.7001 8.60005L10.0001 9.40005L11.6001 11L10.0001 12.6Z" fill="#C5C5C5"/>
+											<path d="M1.00006 4L15.0001 4L15.0001 3L1.00006 3L1.00006 4Z" fill="#C5C5C5"/>
+											<path d="M1.00006 7L15.0001 7L15.0001 6L1.00006 6L1.00006 7Z" fill="#C5C5C5"/>
+											<path d="M9.00006 9.5L9.00006 9L1.00006 9L1.00006 10L9.00006 10L9.00006 9.5Z" fill="#C5C5C5"/>
+											<path d="M9.00006 13L9.00006 12.5L9.00006 12L1.00006 12L1.00006 13L9.00006 13Z" fill="#C5C5C5"/>
+					 </svg>
+				 </button>
+			{/if}
+		</div>
 	</ul>
 	{#if current == "console"}
 		<Console></Console>
@@ -105,6 +118,24 @@ import { code, log } from '../stores';
 			}
 			&.active{
 				border-bottom: 1px solid var(--vscode-panelTitle-activeBorder);
+			}
+		}
+		.right{
+			margin-left: auto;
+			padding-right: 1em;
+			.action-btn{
+				border: none;
+				background: none;
+				padding: 3px;
+    		border-radius: 5px;
+				display: grid;
+				place-items: center;
+				&:hover{
+					background-color: rgba(90, 93, 94, 0.31);
+				}
+				&:focus{
+					outline: none;
+				}
 			}
 		}
 	}
