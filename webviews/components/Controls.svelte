d6a638b58bfe186bd8b75179d0e6beca5b9558de
Rafael <rafael.martinezcalvo@orange.fr>
2021-09-11 09:49:57 +0200

    Controls more responsive
     wrap on different lines

--- webviews/components/Controls.svelte
+++ webviews/components/Controls.svelte
@@ -11,49 +11,51 @@
 </script>
 
 <div class="controls">
-  <label for="lock">
-    lock
-    <Switch id="lock" on:change={updateLock} bind:checked={$locked}/>
-  </label>
-  <label for="center">
-    center
-    <Switch id="center" on:change={update} bind:checked={$config.center} />
-  </label>
-  <label for="background">
-    background
-    <Switch
-      id="background"
-      on:change={update}
-      bind:checked={$config.activeBg}
-    />
-    {#if $config.activeBg}
-      <input type="color" on:change={update} bind:value={$config.bg} />
-    {/if}
-  </label>
-  <label for="zoom">
-    zoom
-    <select name="zoom" on:blur={update} id="zoom" bind:value={$config.zoom}>
-      <option value="0.5">0.5x</option>
-      <option value="1">1x</option>
-      <option value="1.5">1.5x</option>
-      <option value="2">2x</option>
-      <option value="2.5">2.5x</option>
-      <option value="3">3x</option>
-      <option value="4">4x</option>
-      <option value="5">5x</option>
-      <option value="6">6x</option>
-      <option value="7">7x</option>
-      <option value="8">8x</option>
-      <option value="9">9x</option>
-      <option value="10">10x</option>
-      <option value="11">11x</option>
-      <option value="12">12x</option>
-    </select>
-  </label>
-  <label for="rollup">
-    use rollup
-    <Switch id="rollup" on:change={update} bind:checked={$config.rollup} />
-  </label>
+  <div class="wrap">
+    <label for="lock">
+      lock
+      <Switch id="lock" on:change={updateLock} bind:checked={$locked}/>
+    </label>
+    <label for="center">
+      center
+      <Switch id="center" on:change={update} bind:checked={$config.center} />
+    </label>
+    <label for="background">
+      background
+      <Switch
+        id="background"
+        on:change={update}
+        bind:checked={$config.activeBg}
+      />
+      {#if $config.activeBg}
+        <input type="color" on:change={update} bind:value={$config.bg} />
+      {/if}
+    </label>
+    <label for="zoom">
+      zoom
+      <select name="zoom" on:blur={update} id="zoom" bind:value={$config.zoom}>
+        <option value="0.5">0.5x</option>
+        <option value="1">1x</option>
+        <option value="1.5">1.5x</option>
+        <option value="2">2x</option>
+        <option value="2.5">2.5x</option>
+        <option value="3">3x</option>
+        <option value="4">4x</option>
+        <option value="5">5x</option>
+        <option value="6">6x</option>
+        <option value="7">7x</option>
+        <option value="8">8x</option>
+        <option value="9">9x</option>
+        <option value="10">10x</option>
+        <option value="11">11x</option>
+        <option value="12">12x</option>
+      </select>
+    </label>
+    <label for="rollup">
+      use rollup
+      <Switch id="rollup" on:change={update} bind:checked={$config.rollup} />
+    </label>
+  </div>
 
   {#if $code?.startTime}
     <p class="right">
@@ -74,6 +76,11 @@
     display: flex;
     background: var(--vscode-titleBar-activeBackground);
     padding: 1em;
+    .wrap{
+      display: flex;
+      flex-grow: 1;
+      flex-wrap: wrap;
+    }
   }
   select {
     padding: 0.5em;
@@ -88,6 +95,11 @@
     font-size: 1.2em;
     span {
       font-weight: bold;
+    }
+  }
+  @media (max-width: 855px){
+    label {
+      margin-bottom: 1.5em;
     }
   }
 </style>
